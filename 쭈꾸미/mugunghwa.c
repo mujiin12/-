6c8e02b93b1e04589a4dfcc60c3bd346d0866d2f
김무진 <@LAPTOP-D1PSJ5LP>
2023-10-14 12:30:23 +0900

    초기 커밋.

---
+++ 쭈꾸미/mugunghwa.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+#define DIR_UP		0
+#define DIR_DOWN	1
+#define DIR_LEFT	2
+#define DIR_RIGHT	3
+
+void sample_init(void);
+void move_manual(key_t key);
+void move_random(int i, int dir);
+void move_tail(int i, int nx, int ny);
